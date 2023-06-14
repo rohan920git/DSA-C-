89df5060f79dd74a0c2143a5ea6504fe2ac99940
Rohan Baghel <106821116+rohan920git@users.noreply.github.com>
2023-06-14 16:01:15 +0530

    Update flatten binary tree.cpp

--- flatten binary tree.cpp
+++ flatten binary tree.cpp
@@ -26,3 +26,25 @@ public:
         
     }
 };
+// iterative approach
+class Solution {
+public:
+    void flatten(TreeNode* root) {
+        while(root != NULL){
+            if(root -> left == NULL){
+                root = root->right;
+            }
+            else{
+                TreeNode* pred = root->left;
+                while(pred->right != NULL){
+                    pred = pred->right;
+                }
+                pred -> right = root->right;
+                root ->right = root->left;
+                root->left = NULL;                 
+                root = root->right;
+                
+            }
+        }
+    }
+};
